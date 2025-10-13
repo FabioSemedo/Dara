87caacc3f04a43655ecf21b302c79b83badb7443
Fábio <127618255+FabioSemedo@users.noreply.github.com>
2025-10-13 15:51:14 +0100

    Dynamic board generation

---
+++ public/Part1/board.js
@@ -0,0 +1,50 @@
+
+// Game board class
+class GameBoard {
+    constructor(id, cols) {
+        this.content = new Array(cols*4);
+        this.boardElements = new Array(cols*4);
+        this.currentPlayer = 'X';
+        this.cols = cols;
+        
+        const parent = document.getElementById(id);
+        const board = document.createElement('div');
+        
+        board.className = 'board';
+        parent.appendChild(board);
+
+        board.style.gridTemplateRows = `repeat(4, auto)`;
+        board.style.gridTemplateColumns = `repeat(${cols}, auto)`;
+        
+        // Create cells
+        for(let i = 0; i < cols*4; i++) {
+            let cell = document.createElement('div');
+            cell.className = 'board-square';
+            cell.dataset.index = i;
+            board.appendChild(cell);
+
+            if(i<cols || i> 3*cols-1){
+                let token = document.createElement('div');
+                let token_class = (i<cols ?'board-token player-2':'board-token player-1');
+                token.className = token_class;
+                token.dataset.index = i;
+                cell.appendChild(token);
+
+            }
+            
+            // Add click event
+            cell.addEventListener('click', () => this.play(i));
+            
+            this.boardElements[i] = cell;  
+        }
+        
+        
+        // Initialize content array
+        this.content.fill(null);
+    }
+    
+}
+
+window.onload = function() {
+    const game = new GameBoard("base", 15);
+}
